c9e6082ff31a9747ffde832038707163ba460b85
moha88z <mohammed.alayda88@gmail.com>
2025-07-23 00:09:47 +0000

    Python - More Data Structures: Set, Dictionary

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        del a_dicionary[key]
-    return a_dicionary
+        del a_dictionary[key]
+    return a_dictionary
