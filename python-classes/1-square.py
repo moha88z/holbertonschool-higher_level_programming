a7bcf434d6c3909c98fc855fcfc1a7ed2ac9bd87
moha88z <mohammed.alayda88@gmail.com>
2025-07-28 08:31:06 +0000

    Python - Classes and Objects

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 class square:
-    """class that difines a square with a private size."""
+    """
+    class that difines a square with a private size.
+    """
 
-
-    def __inti__(self, size):
+    def __inti__(self, size) -> None:
         self.__size = size
