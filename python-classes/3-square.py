ccf31173b784d15719bd93ac392394c85c9f6599
moha88z <mohammed.alayda88@gmail.com>
2025-07-29 10:34:18 +0000

    Python - Classes and Objects

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains code to manipulate squares
+"""
+
+
+class Square:
+    """
+    This class represents a square
+    """
+
+    def __init__(self, size=0) -> None:
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
