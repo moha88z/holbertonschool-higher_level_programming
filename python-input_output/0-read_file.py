771dd772d7e06d0a867c5c8a6530659bba923d94
moha88z <mohammed.alayda88@gmail.com>
2025-08-11 11:09:55 +0000

    Python - Input/Output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module that defines a function to read a text file and print its contents."""
 def read_file(filename=""):
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
