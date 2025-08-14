ee64805d91de96253f4bfff23d3108351964cf50
moha88z <mohammed.alayda88@gmail.com>
2025-08-14 13:45:44 +0000

    Python - Input/Output

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Generate Pascal's triangle as a list of lists."""
+
+
 def pascal_triangle(n):
+    """Return Pascal’s triangle of size n as a list of lists."""
     if n <= 0:
         return []
 
     triangle = [[1]]
-    for i in range(1, n):
+    for _ in range(1, n):
         prev_row = triangle[-1]
         row = [1]
         for j in range(1, len(prev_row)):
