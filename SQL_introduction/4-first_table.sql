e60008410b7377dba9f3bc31a16a3390e4c16459
moha88z <mohammed.alayda88@gmail.com>
2025-09-01 15:00:17 +0000

    4-first_table.sql

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Task 4: Create a table named first_table
+-- This script creates first_table with columns id (INT) and name (VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
