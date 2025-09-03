fa645e2d64db82cca00790658eb15b5ae353eab1
moha88z <mohammed.alayda88@gmail.com>
2025-09-03 15:19:17 +0000

    10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Task 10: List all records from second_table ordered by best score
+-- This script selects score and name from second_table and orders them descending by score
+SELECT score, name FROM second_table ORDER BY score DESC;
+
