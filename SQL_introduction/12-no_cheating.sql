20836d51ba23d7d203eca75f954d0ac4729dddc5
moha88z <mohammed.alayda88@gmail.com>
2025-09-03 16:39:48 +0000

    12-no_cheating.sql

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- Task 12: Update  Bob's score to 10
+-- This script update the score of Bob in secore of Bob in second_table without using his id
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
+
