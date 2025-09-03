2686aceb603971e84ab6dd66bff457cc16ec8305
moha88z <mohammed.alayda88@gmail.com>
2025-09-03 16:31:38 +0000

    11-best_score.sql

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- Task 11: Select the best scores
+-- This script selects score and name from second_table where score >= 10 and orders them descending
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
+
