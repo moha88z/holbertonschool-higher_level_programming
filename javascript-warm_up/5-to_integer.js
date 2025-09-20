0de6ef2a6d8641ccc7369e6cac1869f627e74294
moha88z <mohammed.alayda88@gmail.com>
2025-09-20 07:49:50 +0000

    5-to_integer.js

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const number = parseInt(arg, 10);
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${number}`);
+}
