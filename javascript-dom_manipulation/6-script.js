25220e1ad328bd4cda0e41365fad93d3124ebc70
moha88z <mohammed.alayda88@gmail.com>
2025-09-25 07:01:34 +0000

    6-script.js

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,9 @@
+const character = document.getElementById('character');
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    character.textContent = data.name;
+  });
